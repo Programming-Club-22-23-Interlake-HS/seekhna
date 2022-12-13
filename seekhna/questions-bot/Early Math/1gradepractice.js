f1493d334d34ed3a1a16c300b7e7f0a5d15ab924
Aarnav Bhat <aarnavpbhat@gmail.com>
2022-12-12 21:02:32 -0800

    Initial Question Edits

--- seekhna/questions-bot/Early Math/1gradepractice.js
+++ seekhna/questions-bot/Early Math/1gradepractice.js
@@ -1,42 +1,42 @@
 const questions = [
     {
-        question: "How many days makes a week ?",
-        optionA: "10 days",
-        optionB: "14 days",
-        optionC: "5 days",
-        optionD: "7 days",
+        question: "1+1?",
+        optionA: "4",
+        optionB: "5",
+        optionC: "3",
+        optionD: "2",
         correctOption: "optionD"
     },
 
     {
-        question: "How many players are allowed on a soccer pitch ?",
-        optionA: "10 players",
-        optionB: "11 players",
-        optionC: "9 players",
-        optionD: "12 players",
+        question: "3+1?",
+        optionA: "3",
+        optionB: "4",
+        optionC: "2",
+        optionD: "1",
         correctOption: "optionB"
     },
 
     {
-        question: "Who was the first President of USA ?",
-        optionA: "Donald Trump",
-        optionB: "Barack Obama",
-        optionC: "Abraham Lincoln",
-        optionD: "George Washington",
+        question: "5-4:?",
+        optionA: "2",
+        optionB: "9",
+        optionC: "6",
+        optionD: "1",
         correctOption: "optionD"
     },
 
     {
-        question: "30 days has ______ ?",
-        optionA: "January",
-        optionB: "December",
-        optionC: "June",
-        optionD: "August",
+        question: "2+1?",
+        optionA: "4",
+        optionB: "2",
+        optionC: "3",
+        optionD: "1",
         correctOption: "optionC"
     },
 
     {
-        question: "How manay hours can be found in a day ?",
+        question: "5-4?",
         optionA: "30 hours",
         optionB: "38 hours",
         optionC: "48 hours",
@@ -45,11 +45,11 @@ const questions = [
     },
 
     {
-        question: "Which is the longest river in the world ?",
-        optionA: "River Nile",
-        optionB: "Long River",
-        optionC: "River Niger",
-        optionD: "Lake Chad",
+        question: "9-2?",
+        optionA: "7",
+        optionB: "11",
+        optionC: "4",
+        optionD: "3",
         correctOption: "optionA"
     },
 
