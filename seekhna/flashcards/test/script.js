5b4743e257ef9beaf05487a5b122da7c84ef3e37
Aarnav Bhat <aarnavpbhat@gmail.com>
2022-12-17 08:40:25 -0800

    Edited column names

--- seekhna/flashcards/test/script.js
+++ seekhna/flashcards/test/script.js
@@ -184,29 +184,29 @@
 
       //global varriables
        let ID = [];
-       let eng1 = [];
-       let eng2 = [];
-       let eng3 = [];
-       let pol1 = [];
-       let pol2 = [];
-       let pol3 = [];
+       let term1 = [];
+       let term2 = [];
+       let term3 = [];
+       let ans1 = [];
+       let ans2 = [];
+       let ans3 = [];
        
-       let eng123 = [];
-       let pol123 = [];
+       let term123 = [];
+       let ans123 = [];
 
 
 
        function clearArrays() {
         ID.splice(0,ID.length);
-        eng1.splice(0,eng1.length);
-        eng2.splice(0,eng2.length);
-        eng3.splice(0,eng3.length);
-        pol1.splice(0,pol1.length);
-        pol2.splice(0,pol2.length);
-        pol3.splice(0,pol3.length);
+        term1.splice(0,term1.length);
+        term2.splice(0,term2.length);
+        term3.splice(0,term3.length);
+        ans1.splice(0,ans1.length);
+        ans2.splice(0,ans2.length);
+        ans3.splice(0,ans3.length);
 
-        eng123.splice(0,eng123.length);
-        pol123.splice(0,pol123.length);
+        term123.splice(0,term123.length);
+        ans123.splice(0,ans123.length);
        }
 
       async function getDataFromDatabase(databaseName, databaseTable) {
@@ -230,50 +230,50 @@
       for (let i=0; i<numberOfRecords; i++) {
         //ID
         ID.push(i+1);
-        //eng1
-        response = db.exec(`SELECT eng1 FROM ${databaseTable} LIMIT 1 OFFSET ${i}`);
+        //term1
+        response = db.exec(`SELECT term1 FROM ${databaseTable} LIMIT 1 OFFSET ${i}`);
         response = response[0].values[0].toString();
-        eng1.push(response);
-        //eng2
-        response = db.exec(`SELECT eng2 FROM ${databaseTable} LIMIT 1 OFFSET ${i}`);
+        term1.push(response);
+        //term2
+        response = db.exec(`SELECT term2 FROM ${databaseTable} LIMIT 1 OFFSET ${i}`);
         response = response[0].values[0].toString();
-        eng2.push(response);
-        //eng3
-        response = db.exec(`SELECT eng3 FROM ${databaseTable} LIMIT 1 OFFSET ${i}`);
+        term2.push(response);
+        //term3
+        response = db.exec(`SELECT term3 FROM ${databaseTable} LIMIT 1 OFFSET ${i}`);
         response = response[0].values[0].toString();
-        eng3.push(response);
-        //pol1
-        response = db.exec(`SELECT pol1 FROM ${databaseTable} LIMIT 1 OFFSET ${i}`);
+        term3.push(response);
+        //ans1
+        response = db.exec(`SELECT ans1 FROM ${databaseTable} LIMIT 1 OFFSET ${i}`);
         response = response[0].values[0].toString();
-        pol1.push(response);
-        //pol2
-        response = db.exec(`SELECT pol2 FROM ${databaseTable} LIMIT 1 OFFSET ${i}`);
+        ans1.push(response);
+        //ans2
+        response = db.exec(`SELECT ans2 FROM ${databaseTable} LIMIT 1 OFFSET ${i}`);
         response = response[0].values[0].toString();
-        pol2.push(response);
-        //pol3
-        response = db.exec(`SELECT pol3 FROM ${databaseTable} LIMIT 1 OFFSET ${i}`);
+        ans2.push(response);
+        //ans3
+        response = db.exec(`SELECT ans3 FROM ${databaseTable} LIMIT 1 OFFSET ${i}`);
         response = response[0].values[0].toString();
-        pol3.push(response);
+        ans3.push(response);
 
-        //eng123
-        if (eng1[i] != "" && eng2[i] != "" && eng3[i] != "") {
-          eng123.push(`${eng1[i]} / ${eng2[i]} / ${eng3[i]}`);
+        //term123
+        if (term1[i] != "" && term2[i] != "" && term3[i] != "") {
+          term123.push(`${term1[i]} / ${term2[i]} / ${term3[i]}`);
         }
-        else if (eng1[i] != "" && eng2[i] != "") {
-          eng123.push(`${eng1[i]} / ${eng2[i]}`);
-        }
-        else {
-          eng123.push(`${eng1[i]}`);
-        }
-        //pol123
-        if (pol1[i] != "" && pol2[i] != "" && pol3[i] != "") {
-          pol123.push(`${pol1[i]} / ${pol2[i]} / ${pol3[i]}`);
-        }
-        else if (pol1[i] != "" && pol2[i] != "") {
-          pol123.push(`${pol1[i]} / ${pol2[i]}`);
+        else if (term1[i] != "" && term2[i] != "") {
+          term123.push(`${term1[i]} / ${term2[i]}`);
         }
         else {
-          pol123.push(`${pol1[i]}`);
+          term123.push(`${term1[i]}`);
+        }
+        //ans123
+        if (ans1[i] != "" && ans2[i] != "" && ans3[i] != "") {
+          ans123.push(`${ans1[i]} / ${ans2[i]} / ${ans3[i]}`);
+        }
+        else if (ans1[i] != "" && ans2[i] != "") {
+          ans123.push(`${ans1[i]} / ${ans2[i]}`);
+        }
+        else {
+          ans123.push(`${ans1[i]}`);
         }
 
       }
@@ -294,8 +294,8 @@
         learnpageTable.innerHTML += `
         <tr>
         <td>${ID[i]}</td>
-        <td>${eng123[i]}</td>
-        <td>${pol123[i]}</td>
+        <td>${term123[i]}</td>
+        <td>${ans123[i]}</td>
         </tr>
         `;
         }
@@ -430,7 +430,7 @@
       let correctAnswers = 0;
       let uncorrectAnswers = 0;
       const checkCorrectnessOfTranslation = () => {
-        if (userTranslation.value != '' && (userTranslation.value === eng1[randomNumberOfRecord] || userTranslation.value === eng2[randomNumberOfRecord] || userTranslation.value === eng3[randomNumberOfRecord]))
+        if (userTranslation.value != '' && (userTranslation.value === term1[randomNumberOfRecord] || userTranslation.value === term2[randomNumberOfRecord] || userTranslation.value === term3[randomNumberOfRecord]))
         {
           correctAnswers ++;
           showCard('green');
@@ -447,7 +447,7 @@
       const showCard = (type) => {
         switch (type) {
           case 'check':
-            checkCardOriginalMeaning.textContent = pol123[randomNumberOfRecord];
+            checkCardOriginalMeaning.textContent = ans123[randomNumberOfRecord];
             userTranslation.value = '';
             userTranslation.focus(); //automatically set cursor in input
             btnCheck.innerHTML = 'Check (Enter)';
@@ -456,9 +456,9 @@
           break;
 
           case 'red':
-            resultCardOriginalMeaning.textContent = pol123[randomNumberOfRecord];
+            resultCardOriginalMeaning.textContent = ans123[randomNumberOfRecord];
             resultCardUserTranslation.textContent = `❌ ${userTranslation.value}`;
-            resultCardParagraph.textContent = `✅ ${eng123[randomNumberOfRecord]}`;
+            resultCardParagraph.textContent = `✅ ${term123[randomNumberOfRecord]}`;
             resultCard.style.background = '#751b1b'; //red
             btnCheck.innerHTML = 'Try again (Enter)';
             cardContent.style.transform='rotateY(-180deg)';
@@ -466,8 +466,8 @@
           break;
 
           case 'green':
-            resultCardOriginalMeaning.textContent = pol123[randomNumberOfRecord];
-            resultCardUserTranslation.textContent = `✅ ${eng123[randomNumberOfRecord]}`;
+            resultCardOriginalMeaning.textContent = ans123[randomNumberOfRecord];
+            resultCardUserTranslation.textContent = `✅ ${term123[randomNumberOfRecord]}`;
             resultCardParagraph.textContent = "";
             resultCard.style.background = '#0f6e35'; //green
             btnCheck.innerHTML = 'Next card (Enter)';
